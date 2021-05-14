33f8e9d5151524d7d5d4da52660d93e06c36baa3
antho1404 <anthony.estebe@gmail.com>
2021-05-14 17:01:20 +0700

    create demo to connect to the wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { Web3ReactProvider } from '@web3-react/core'
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  function getLibrary(provider: any): Web3Provider {
+    return new Web3Provider(provider)
+  }
+
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
