37b9f8898b2f9cd9a8fb633ea23fc1eecc378c54
gopherquiv <gopher@quiverprotocol.com>
2021-05-17 02:16:20 +1000

    change the order of images by emotion index

--- pages/data/index.tsx
+++ pages/data/index.tsx
@@ -47,286 +47,286 @@ export const characters = [
     {
         name: "Diamond Bull",
         images: [
-           "characters/bull/diamond/happy.png",
            "characters/bull/diamond/angry.png",
            "characters/bull/diamond/worry.png",
            "characters/bull/diamond/normal.png",
-           "characters/bull/diamond/rest.png"
+           "characters/bull/diamond/rest.png",
+           "characters/bull/diamond/happy.png",
         ],
         artist: artists.clive,
     },
     {
         name: "Bronze Bull",
         images: [
-           "characters/bull/bronze/happy.png",
            "characters/bull/bronze/angry.png",
            "characters/bull/bronze/worry.png",
            "characters/bull/bronze/normal.png",
-           "characters/bull/bronze/rest.png"
+           "characters/bull/bronze/rest.png",
+           "characters/bull/bronze/happy.png",
         ],
         artist: artists.clive,
     },
     {
         name: "Silver Bull",
         images: [
-           "characters/bull/silver/happy.png",
            "characters/bull/silver/angry.png",
            "characters/bull/silver/worry.png",
            "characters/bull/silver/normal.png",
-           "characters/bull/silver/rest.png"
+           "characters/bull/silver/rest.png",
+           "characters/bull/silver/happy.png",
         ],
         artist: artists.clive,
     },
     {
         name: "Golden Bull",
         images: [
-            "characters/bull/gold/happy.png",
             "characters/bull/gold/angry.png",
             "characters/bull/gold/worry.png",
             "characters/bull/gold/normal.png",
-            "characters/bull/gold/rest.png"
+            "characters/bull/gold/rest.png",
+            "characters/bull/gold/happy.png",
          ],
          artist: artists.clive,
     },
     {
         name: "Platium Bull",
         images: [
-            "characters/bull/platium/happy.png",
             "characters/bull/platium/angry.png",
             "characters/bull/platium/worry.png",
             "characters/bull/platium/normal.png",
-            "characters/bull/platium/rest.png"
+            "characters/bull/platium/rest.png",
+            "characters/bull/platium/happy.png",
          ],
          artist: artists.clive,
     },
     {
         name: "Diamond Bear",
         images: [
-           "characters/bear/diamond/happy.png",
            "characters/bear/diamond/angry.png",
            "characters/bear/diamond/worry.png",
            "characters/bear/diamond/normal.png",
-           "characters/bear/diamond/rest.png"
+           "characters/bear/diamond/rest.png",
+           "characters/bear/diamond/happy.png",
         ],
         artist: artists.rogan
     },
     {
         name: "Bronze Bear",
         images: [
-            "characters/bear/bronze/happy.png",
             "characters/bear/bronze/angry.png",
             "characters/bear/bronze/worry.png",
             "characters/bear/bronze/normal.png",
-            "characters/bear/bronze/rest.png"
+            "characters/bear/bronze/rest.png",
+            "characters/bear/bronze/happy.png",
          ],
          artist: artists.rogan
     },
     {
         name: "Silver Bear",
         images: [
-           "characters/bear/silver/happy.png",
            "characters/bear/silver/angry.png",
            "characters/bear/silver/worry.png",
            "characters/bear/silver/normal.png",
-           "characters/bear/silver/rest.png"
+           "characters/bear/silver/rest.png",
+           "characters/bear/silver/happy.png",
         ],
         artist: artists.rogan
     },
     {
         name: "Golden Bear",
         images: [
-            "characters/bear/gold/happy.png",
             "characters/bear/gold/angry.png",
             "characters/bear/gold/worry.png",
             "characters/bear/gold/normal.png",
-            "characters/bear/gold/rest.png"
+            "characters/bear/gold/rest.png",
+            "characters/bear/gold/happy.png",
         ],
         artist: artists.rogan
     },
     {
         name: "Platium Bear",
         images: [
-            "characters/bear/platium/happy.png",
             "characters/bear/platium/angry.png",
             "characters/bear/platium/worry.png",
             "characters/bear/platium/normal.png",
-            "characters/bear/platium/rest.png"
+            "characters/bear/platium/rest.png",
+            "characters/bear/platium/happy.png",
         ],
         artist: artists.rogan
     },
     {
         name: "Diamond Whale",
         images: [
-            "characters/whale/diamond/happy.png",
             "characters/whale/diamond/angry.png",
             "characters/whale/diamond/worry.png",
             "characters/whale/diamond/normal.png",
-            "characters/whale/diamond/rest.png"
+            "characters/whale/diamond/rest.png",
+            "characters/whale/diamond/happy.png",
         ],
         artist: artists.keili
     },
     {
         name: "Bronze Whale",
         images: [
-            "characters/whale/bronze/happy.png",
             "characters/whale/bronze/angry.png",
             "characters/whale/bronze/worry.png",
             "characters/whale/bronze/normal.png",
-            "characters/whale/bronze/rest.png"
+            "characters/whale/bronze/rest.png",
+            "characters/whale/bronze/happy.png",
         ],
         artist: artists.keili
     },
     {
         name: "Silver Whale",
         images: [
-            "characters/whale/silver/happy.png",
             "characters/whale/silver/angry.png",
             "characters/whale/silver/worry.png",
             "characters/whale/silver/normal.png",
-            "characters/whale/silver/rest.png"
+            "characters/whale/silver/rest.png",
+            "characters/whale/silver/happy.png",
         ],
         artist: artists.keili
     },
     {
         name: "Golden Whale",
         images: [
-            "characters/whale/gold/happy.png",
             "characters/whale/gold/angry.png",
             "characters/whale/gold/worry.png",
             "characters/whale/gold/normal.png",
-            "characters/whale/gold/rest.png"
+            "characters/whale/gold/rest.png",
+            "characters/whale/gold/happy.png",
         ],
         artist: artists.keili
     },
     {
         name: "Platium Whale",
         images: [
-            "characters/whale/platium/happy.png",
             "characters/whale/platium/angry.png",
             "characters/whale/platium/worry.png",
             "characters/whale/platium/normal.png",
-            "characters/whale/platium/rest.png"
+            "characters/whale/platium/rest.png",
+            "characters/whale/platium/happy.png",
         ],
         artist: artists.keili
     },
     {
         name: "Diamond Dragon",
         images: [
-            "characters/dragon/diamond/happy.png",
             "characters/dragon/diamond/angry.png",
             "characters/dragon/diamond/worry.png",
             "characters/dragon/diamond/normal.png",
-            "characters/dragon/diamond/rest.png"
+            "characters/dragon/diamond/rest.png",
+            "characters/dragon/diamond/happy.png",
         ],
         artist: artists.jatin
     },
     {
         name: "Bronze Dragon",
         images: [
-            "characters/dragon/bronze/happy.png",
             "characters/dragon/bronze/angry.png",
             "characters/dragon/bronze/worry.png",
             "characters/dragon/bronze/normal.png",
-            "characters/dragon/bronze/rest.png"
+            "characters/dragon/bronze/rest.png",
+            "characters/dragon/bronze/happy.png",
         ],
         artist: artists.jatin
     },
     {
         name: "Silver Dragon",
         images: [
-            "characters/dragon/silver/happy.png",
             "characters/dragon/silver/angry.png",
             "characters/dragon/silver/worry.png",
             "characters/dragon/silver/normal.png",
-            "characters/dragon/silver/rest.png"
+            "characters/dragon/silver/rest.png",
+            "characters/dragon/silver/happy.png",
         ],
         artist: artists.jatin
     },
     {
         name: "Golden Dragon",
         images: [
-            "characters/dragon/gold/happy.png",
             "characters/dragon/gold/angry.png",
             "characters/dragon/gold/worry.png",
             "characters/dragon/gold/normal.png",
-            "characters/dragon/gold/rest.png"
+            "characters/dragon/gold/rest.png",
+            "characters/dragon/gold/happy.png",
         ],
         artist: artists.jatin
     },
     {
         name: "Platium Dragon",
         images: [
-            "characters/dragon/platium/happy.png",
             "characters/dragon/platium/angry.png",
             "characters/dragon/platium/worry.png",
             "characters/dragon/platium/normal.png",
-            "characters/dragon/platium/rest.png"
+            "characters/dragon/platium/rest.png",
+            "characters/dragon/platium/happy.png",
         ],
         artist: artists.jatin
     },
     {
         name: "Diamond Deer",
         images: [
-            "characters/deer/diamond/happy.png",
             "characters/deer/diamond/angry.png",
             "characters/deer/diamond/worry.png",
             "characters/deer/diamond/normal.png",
-            "characters/deer/diamond/rest.png"
+            "characters/deer/diamond/rest.png",
+            "characters/deer/diamond/happy.png",
         ],
         artist: artists.mehak
     },
     {
         name: "Bronze Deer",
         images: [
-            "characters/deer/bronze/happy.png",
             "characters/deer/bronze/angry.png",
             "characters/deer/bronze/worry.png",
             "characters/deer/bronze/normal.png",
-            "characters/deer/bronze/rest.png"
+            "characters/deer/bronze/rest.png",
+            "characters/deer/bronze/happy.png",
         ],
         artist: artists.mehak
     },
     {
         name: "Silver Deer",
         images: [
-            "characters/deer/silver/happy.png",
             "characters/deer/silver/angry.png",
             "characters/deer/silver/worry.png",
             "characters/deer/silver/normal.png",
-            "characters/deer/silver/rest.png"
+            "characters/deer/silver/rest.png",
+            "characters/deer/silver/happy.png",
         ],
         artist: artists.mehak
     },
     {
         name: "Golden Deer",
         images: [
-            "characters/deer/gold/happy.png",
             "characters/deer/gold/angry.png",
             "characters/deer/gold/worry.png",
             "characters/deer/gold/normal.png",
-            "characters/deer/gold/rest.png"
+            "characters/deer/gold/rest.png",
+            "characters/deer/gold/happy.png",
         ],
         artist: artists.mehak
     },
     {
         name: "Platium Deer",
         images: [
-            "characters/deer/platium/happy.png",
             "characters/deer/platium/angry.png",
             "characters/deer/platium/worry.png",
             "characters/deer/platium/normal.png",
-            "characters/deer/platium/rest.png"
+            "characters/deer/platium/rest.png",
+            "characters/deer/platium/happy.png",
         ],
         artist: artists.mehak
     },
     {
         name: "Fish",
         images: [
-            "characters/fish/happy.png",
             "characters/fish/angry.png",
             "characters/fish/worry.png",
             "characters/fish/normal.png",
-            "characters/fish/rest.png"
+            "characters/fish/rest.png",
+            "characters/fish/happy.png",
         ],
         artist: artists.debbie
     },
