b611827c8ea572e2e4abcbf9daab29469252b1cc
antho1404 <anthony.estebe@gmail.com>
2021-05-18 10:36:17 +0700

    add pages

---
+++ pages/wallet.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Wallet(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Wallet</title>
+      </Head>
+
+      <main>
+        <ul>
+          <li>NFT1</li>
+          <li>NFT2</li>
+          <li>NFT3</li>
+        </ul>
+      </main>
+    </>
+  )
+}
