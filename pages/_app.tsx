bc2f82f0d09658653272560ccf7446f10399e7b6
antho1404 <anthony.estebe@gmail.com>
2021-05-18 10:35:53 +0700

    create general layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app'
+import Navigation from '../components/navigation/navigation'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <div className="relative min-h-screen flex flex-col">
+      <Navigation />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex items-stretch overflow-hidden">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
