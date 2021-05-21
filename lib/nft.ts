07a6d3ba5ee543fe9eb563a414ff5bd3cae4cf24
Nicolas Mahe <nicolas@mahe.me>
2021-05-21 19:16:05 +0700

    add api nfts get

--- lib/nft.ts
+++ lib/nft.ts
@@ -1,6 +1,24 @@
-import { animals, characters, skins } from '../data/nft'
-import { Creature, Metadata, Skin, Traits } from '../types/metadata'
+import {
+  animals,
+  backgrounds,
+  characters,
+  favCoins,
+  lockOptions,
+  skins,
+} from '../data/nft'
+import { APINftMetadataResponse } from '../types/api'
+import { QNFT } from '../types/contracts'
+import {
+  Creature,
+  HydratedMetadata,
+  Metadata,
+  MetadataOffChain,
+  MetadataOnChain,
+  Skin,
+  Traits,
+} from '../types/metadata'
 import { Character } from '../types/nft'
+import { supabase } from './supabase'
 
 export const attribute = (
   metadata: APINftMetadataResponse,
@@ -19,4 +37,68 @@ export const getCreature = (
   const skinIndex = skins.findIndex((x) => x === skin)
   const id = animalIndex * skins.length + skinIndex
   return characters.find((x) => x.id === id)
+}
+
+// fetches metadata from on-chain and off-chain
+export const fetchMetadata = async (
+  qnftContract: QNFT,
+  tokenId: number,
+): Promise<Metadata> => {
+  // // fetch info on-chain
+  const nftDataOnChain = (await qnftContract.nftData(
+    tokenId,
+  )) as MetadataOnChain
+
+  // fetch info off-chain from database
+  const { data, error } = await supabase
+    .from('nft')
+    .select('*')
+    .eq('id', nftDataOnChain.metaId.toString())
+  if (error) throw error
+  if (!data || data?.length === 0)
+    throw new Error(
+      `metadata with id "${nftDataOnChain.metaId.toString()}" not found`,
+    )
+  const nftDataOffChain = data.pop() as MetadataOffChain
+
+  return {
+    ...nftDataOnChain,
+    ...nftDataOffChain,
+  }
+}
+
+// put all related structure into the metadata
+export const hydrateMetadata = (metadata: Metadata): HydratedMetadata => {
+  // character
+  const character = characters[metadata.characterId]
+  if (!character)
+    throw new Error(`character with id ${metadata.characterId} not found`)
+
+  // favCoin
+  const favCoin = favCoins[metadata.favCoinId]
+  if (!favCoin)
+    throw new Error(`favCoin with id ${metadata.favCoinId} not found`)
+
+  // lockOption
+  const lockOption = lockOptions.find((x) =>
+    metadata.lockDuration.eq(x.duration),
+  )
+  if (!lockOption)
+    throw new Error(
+      `lockOption with duration ${metadata.lockDuration} not found`,
+    )
+
+  // backgroundUrl
+  const backgroundUrl = backgrounds[metadata.backgroundId]
+  if (!backgroundUrl)
+    throw new Error(`background with id ${metadata.backgroundId} not found`)
+
+  // finally
+  return {
+    ...metadata,
+    character: characters[metadata.characterId],
+    favCoin,
+    lockOption,
+    backgroundUrl,
+  }
 }
