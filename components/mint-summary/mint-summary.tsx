62b5d051814fc7000dc4f8d031665385f8b3d331
antho1404 <anthony.estebe@gmail.com>
2021-05-28 16:13:04 +0700

    update summary component

--- components/mint-summary/mint-summary.tsx
+++ components/mint-summary/mint-summary.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 import { FunctionComponent, HTMLAttributes } from 'react'
-import styles from './mint-summary.module.css'
 import PropertyView, { Property } from './property-view'
 
 export type IProps = HTMLAttributes<{}> & {
@@ -15,17 +14,28 @@ const MintSummary: FunctionComponent<IProps> = ({
   children,
 }: IProps) => {
   return (
-    <div className={classNames(className, styles.mintSummary, 'mb-auto')}>
-      <div className={styles.properties}>
-        <div className={styles.title}>Mint Summary</div>
+    <div
+      className={classNames(
+        className,
+        'bg-white shadow border-purple-100 p-4 rounded-2xl',
+      )}
+    >
+      <div className="p-4">
+        <div className="text-base leading-6 font-bold text-purple-900 mb-8">
+          Mint Summary
+        </div>
         {properties.map((property) => {
           return <PropertyView key={property.title} value={property} />
         })}
       </div>
-      <div className={styles.mint}>
-        <div className={styles.keyValue}>
-          <div className={styles.key}>NFT Mint Price</div>
-          <div className={styles.value}>{mintPrice}</div>
+      <div className="mt-4 rounded-2xl bg-purple-50 p-4">
+        <div className="grid grid-cols-2 gap-2 mb-4">
+          <div className="text-sm leading-5 font-semibold text-purple-900">
+            NFT Mint Price
+          </div>
+          <div className="text-sm leading-5 font-semibold text-purple-700 text-right">
+            {mintPrice}
+          </div>
         </div>
         {children}
       </div>
