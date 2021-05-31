fe1b2b952b2019d986c4b0d690157904bf6865cc
antho1404 <anthony.estebe@gmail.com>
2021-05-31 23:31:23 +0700

    add banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { Web3Provider } from '@ethersproject/providers'
 import { Web3ReactProvider } from '@web3-react/core'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Banner from '../components/navigation/banner'
 import Navigation from '../components/navigation/navigation'
 import WalletGuard from '../components/wallet/guard'
 import '../styles/globals.css'
@@ -12,9 +14,11 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   }
 
   const { route } = useRouter()
+  const [banner, setBanner] = useState(true)
 
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+      {banner && <Banner close={() => setBanner(false)} />}
       <Navigation route={route} className="mb-4" />
       <div className="max-w-7xl mx-auto relative min-h-screen flex flex-col">
         <div className="flex-1 flex flex-col overflow-hidden">
