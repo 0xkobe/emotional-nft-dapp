dfc3e189aab8b3710bdc54a5f811adb436579b5b
gopherquiv <gopher@quiverprotocol.com>
2021-06-01 03:48:34 +1000

    move code to guard

--- components/wallet/guard.tsx
+++ components/wallet/guard.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, PropsWithChildren } from 'react'
 import { chain } from '../../data/chains'
 import useWallet from '../../hooks/useWallet'
-import Button from '../button/button'
+import ModalMetamask from '../modal/modal-metamask'
 
 const WalletGuard: FunctionComponent<PropsWithChildren<any>> = (
   props: PropsWithChildren<any>,
@@ -10,40 +10,61 @@ const WalletGuard: FunctionComponent<PropsWithChildren<any>> = (
 
   if (error)
     return (
-      <div className="max-w-4xl mx-auto bg-white border border-purple-100 rounded-2xl shadow-sm p-8">
-        <div className="text-base leading-6 font-bold text-purple-900">
-          {error.name} {error.message}
-        </div>
-      </div>
+      <ModalMetamask
+        title={error.name}
+        content={<>{error.message}</>}
+        onRequestClose={() => console.error('cannot close this modal')}
+        onModalClose={() => console.error('cannot close this modal')}
+        isShown
+      >
+      </ModalMetamask>
     )
 
   if (!hasWallet)
     return (
-      <div className="max-w-4xl mx-auto bg-white border border-purple-100 rounded-2xl shadow-sm p-8">
-        <div className="text-base leading-6 font-bold text-purple-900 mb-4">
-          Please install Metamask
-        </div>
-        <Button target="_blank" href="https://metamask.io/download">
-          Download metamask
-        </Button>
-      </div>
+      <ModalMetamask
+        title="You Need an Ethereum Wallet"
+        content={<>To use Quiver Emotional NFTs DApp you need to install a MetaMask wallet.</>}
+        onRequestClose={() => console.error('cannot close this modal')}
+        onModalClose={() => console.error('cannot close this modal')}
+        isShown
+      >
+        <a
+          className="text-sm leading-5 font-medium shadow rounded-2xl px-8 py-3 w-full text-center block bg-purple-700 text-white"
+          target="_blank"
+          href="https://metamask.io/download"
+        >
+          Get MetaMask
+        </a>
+      </ModalMetamask>
     )
   if (!account)
     return (
-      <div className="max-w-4xl mx-auto bg-white border border-purple-100 rounded-2xl shadow-sm p-8">
-        <div className="text-base leading-6 font-bold text-purple-900 mb-4">
-          Please connect Metamask
-        </div>
-        <Button onClick={activate}>Activate</Button>
-      </div>
+      <ModalMetamask
+        title="Connect to Your Wallet"
+        content={<>To use Quiver Emotional NFTs DApp you need to sign in to your MetaMask wallet.</>}
+        onRequestClose={() => console.error('cannot close this modal')}
+        onModalClose={() => console.error('cannot close this modal')}
+        isShown
+      >
+        <a
+          onClick={() => activate()}
+          className="text-sm leading-5 font-medium shadow rounded-2xl px-8 py-3 w-full text-center block bg-purple-700 text-white"
+        >
+          Sign In
+        </a>
+      </ModalMetamask>
     )
   if (chainId !== chain.id)
     return (
-      <div className="max-w-4xl mx-auto bg-white border border-purple-100 rounded-2xl shadow-sm p-8">
-        <div className="text-base leading-6 font-bold text-purple-900">
-          Please select {chain.name}
-        </div>
-      </div>
+      <ModalMetamask
+        title="Wrong network"
+        content={<>Wrong chain selected. please switch to {chain.name}</>}
+        onRequestClose={() => console.error('cannot close this modal')}
+        onModalClose={() => console.error('cannot close this modal')}
+        isShown
+      >
+      </ModalMetamask>
     )
 
   return props.children
