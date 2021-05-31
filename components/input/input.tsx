494c89ca6848c92a49a40434191494de0650b5ff
antho1404 <anthony.estebe@gmail.com>
2021-05-31 19:20:31 +0700

    update title and inputs

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { FunctionComponent, InputHTMLAttributes } from 'react'
+import { FunctionComponent,InputHTMLAttributes } from 'react'
 
 export type IProps = InputHTMLAttributes<{}> & {
   unit?: string
@@ -15,7 +15,7 @@ const Input: FunctionComponent<IProps> = ({
   return (
     <input
       className={classNames(
-        'border border-purple-100 shadow rounded-2xl text-sm leading-5 font-normal text-purple-900 px-4 py-2 outline-none placeholder-purple-200 focus:ring-2 ring-purple-400',
+        'border border-purple-100 shadow rounded-2xl text-sm leading-5 font-normal text-purple-900 px-4 py-2 outline-none placeholder-gray-400 focus:ring-2 ring-purple-400',
         isError ? 'border-red-500' : 'border-gray-300',
       )}
       {...props}
