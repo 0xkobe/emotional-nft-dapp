654be16e278370a65eefeee5e3cf6a1e6881c450
Nicolas Mahe <nicolas@mahe.me>
2021-06-02 16:40:43 +0700

    display error in modal in page wallet

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 import LockedTokenStat from '../components/allocation/locked-token-stat'
 import Button from '../components/button/button'
 import IconCardView from '../components/icon/cardview'
+import ModalError from '../components/modal/modal-error'
 import NFTCard from '../components/nft/card'
 import Title from '../components/title/title'
 import { abi, deployedAddresses } from '../data/smartContract'
@@ -12,28 +13,27 @@ import useUserNFTs from '../hooks/useUserNFTs'
 import { fetchPercentages } from '../lib/coingecko'
 
 export default function Wallet(): JSX.Element {
-  const {
-    nfts,
-    error: contractError,
-    isLoading,
-  } = useUserNFTs(deployedAddresses.qnft, abi.qnft)
+  const { nfts, error, isLoading } = useUserNFTs(
+    deployedAddresses.qnft,
+    abi.qnft,
+  )
 
   const [lockAmount, setLockAmount] = useState(BigNumber.from(0))
   const [priceChanges, setPriceChanges] = useState<number[]>([])
 
   useEffect(() => {
-    if (isLoading || contractError || nfts.length === 0) return
+    if (isLoading || error || nfts.length === 0) return
     const sum = nfts.reduce(
       (sum, nft) => sum.add(nft.lockAmount),
       BigNumber.from(0),
     )
     setLockAmount(sum)
-  }, [contractError, isLoading, nfts])
+  }, [error, isLoading, nfts])
 
   useEffect(() => {
-    if (isLoading || contractError || nfts.length === 0) return
+    if (isLoading || error || nfts.length === 0) return
     fetchPercentages(nfts).then(setPriceChanges).catch(console.error)
-  }, [contractError, isLoading, nfts])
+  }, [error, isLoading, nfts])
 
   return (
     <>
@@ -57,8 +57,15 @@ export default function Wallet(): JSX.Element {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-8 lg:grid-cols-4">
+            {error && (
+              <ModalError
+                error={error}
+                onRequestClose={() => window.location.reload()}
+                onModalClose={() => {}}
+                isShown={true}
+              />
+            )}
             {isLoading && <div>loading...</div>}
-            {contractError && <div>{contractError.toString()}</div>}
             {!isLoading && nfts.length === 0 && <div>No NFTs</div>}
             {!isLoading &&
               nfts.map((nft, i) => (
