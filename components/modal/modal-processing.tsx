a417008cf429ae5a0ef5cf5000848d278cebb1fd
Nicolas Mahe <nicolas@mahe.me>
2021-06-02 17:26:52 +0700

    use loader component in modal proccesing

--- components/modal/modal-processing.tsx
+++ components/modal/modal-processing.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, HTMLAttributes } from 'react'
 import { chain } from '../../data/chains'
-import IconSpinner from '../icon/spinner'
+import Loader from '../loader/loader'
 import Modal from './modal'
 
 export type IProps = HTMLAttributes<{}> & {
@@ -22,35 +22,27 @@ const ModalProcessing: FunctionComponent<IProps> = ({
       onModalClose={onModalClose}
       onRequestClose={onRequestClose}
     >
-      <div className="flex flex-col space-y-8 w-80 text-center">
-        <div className="flex justify-center">
-          <IconSpinner />
+      <Loader title="Processing the Transaction">
+        <div className="flex flex-col">
+          <span className="text-sm leading-5 font-normal text-gray-500">
+            Transaction submitted with hash:
+          </span>
+          <span className="text-sm leading-5 font-medium text-gray-500 truncate">
+            {transactionHash}
+          </span>
         </div>
-        <div className="flex flex-col space-y-4">
-          <h1 className="text-lg leading-6 font-semibold text-purple-900">
-            Processing the Transaction
-          </h1>
-          <div className="flex flex-col">
-            <span className="text-sm leading-5 font-normal text-gray-500">
-              Transaction submitted with hash:
-            </span>
-            <span className="text-sm leading-5 font-medium text-gray-500 truncate">
-              {transactionHash}
-            </span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-sm leading-5 font-normal text-gray-500">
-              View details on Etherscan:
-            </span>
-            <a
-              href={chain.explorerUrlForTx(transactionHash)}
-              className="text-sm leading-5 font-medium text-purple-700 truncate"
-            >
-              {chain.explorerUrlForTx(transactionHash)}
-            </a>
-          </div>
+        <div className="flex flex-col">
+          <span className="text-sm leading-5 font-normal text-gray-500">
+            View details on Etherscan:
+          </span>
+          <a
+            href={chain.explorerUrlForTx(transactionHash)}
+            className="text-sm leading-5 font-medium text-purple-700 truncate"
+          >
+            {chain.explorerUrlForTx(transactionHash)}
+          </a>
         </div>
-      </div>
+      </Loader>
     </Modal>
   )
 }
