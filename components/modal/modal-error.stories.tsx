ea8837d089394bf8a6c720698ff3ffe09a65c933
Nicolas Mahe <nicolas@mahe.me>
2021-06-03 14:39:14 +0700

    format

--- components/modal/modal-error.stories.tsx
+++ components/modal/modal-error.stories.tsx
@@ -1,5 +1,5 @@
-import { Meta,Story } from '@storybook/react'
-import ModalError,{ IProps } from './modal-error'
+import { Meta, Story } from '@storybook/react'
+import ModalError, { IProps } from './modal-error'
 
 export default {
   title: 'Modal/ModalError',
